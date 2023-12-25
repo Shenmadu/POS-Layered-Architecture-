275b7adc9430159b2096ca9dfe4dbf02fb078de6
shenmadu <tashen.madu@gmail.com>
2023-12-25 09:51:03 +0530

    use hibernate orderDetailsDao

---
+++ src/main/java/entity/OrderDetailsKey.java
@@ -0,0 +1,22 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class OrderDetailsKey implements Serializable {
+    @Column(name = "orderId")
+    private String orderId;
+    @Column(name = "itemCode")
+    private String itemCode;
+}
